126477c7add6298addc1f6e9893e77071fda0097
Mane-Harshad <harshad.mane2108@gmail.com>
2026-03-27 01:54:38 +0530

    Solve reverseString exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = '';
+    let n=string.length;
+    for (let i = n - 1; i >= 0; i--)
+     {reversedString += string.at(n-1);
+      n=n-1;}
 
+       //   Other ways to reverse a string:-       
+      //  reversedString += string[i];}
+     //  return string.split("").reverse().join(""); --> Odin's solution
+    //   reversedString += string.at(i); --> Which eliminates use of "n--" above
+    return reversedString;
 };
 
 // Do not edit below this line
