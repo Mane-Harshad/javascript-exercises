7deb24541cb1d8f938ac03ffdeaffe9790cdcb9f
Mane-Harshad <harshad.mane2108@gmail.com>
2026-03-28 00:33:46 +0530

    Solve removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,31 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) 
+{return array.filter(item => !args.includes(item));
+    // 1) for (let i = 0; i < args.length; i++)
+    //    {// for (let j = 0; j < array.length; j++)
+    //     {// if (args[i] === array[j])
+    //      {// array.splice(j, 1);
+    //       j--;}}}
+    // return array;
+
+    // 2) for (let i = 0; i < args.length; i++)             /*
+    //     {let index = array.indexOf(args[i]);
+    //      if (index !== -1)                                   Does not work for multiple of the same value, as indexOf only returns the first index of the value
+    //       {array.splice(index, 1);}}
+    //    return array;                                     */
+
+    // 3) args.forEach(item => {                            /*
+    //     const index = array.indexOf(item);
+    //     if (index !== -1)                                    Does not work for multiple of the same value, as indexOf only returns the first index of the value
+    //      {array.splice(index, 1);}
+    //   });
+    //   return array;                                      */  
+
+    // 4) const newArray = [];
+    //    array.forEach(item => {
+    //     if (!args.includes(item)) 
+    //      {newArray.push(item);}
+    //   });
+    //   return newArray;
 };
 
 // Do not edit below this line
