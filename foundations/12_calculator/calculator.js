cba83376a576bd28c67399578ce47bb670d6e885
Mane-Harshad <harshad.mane2108@gmail.com>
2026-03-29 01:52:15 +0530

    Solve calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a,b) {
+ return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  return a-b;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, num) => total + num, 0);
+  //2)  total = 0;
+  //    for (let i = 0; i < array.length; i++) 
+  //     {total += array[i];}
+  //    return total;
+
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, num) => total * num, 1);
+  //2) total = 1;
+  //   for (let i = 0; i < array.length; i++) 
+  //    {total *= array[i];}
+  //   return total;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+  return Math.pow(x,y);
+  // 2) return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n===0)
+   {return 1;}
+
+  let result = 1;
+  for (let i = 1; i <= n; i++)
+   {result *= i;}
+  return result;
+  // 2) if (n === 0)
+  //     {return 1;} 
+  //    else 
+  //     {return n * factorial(n - 1);}   
 };
 
 // Do not edit below this line
