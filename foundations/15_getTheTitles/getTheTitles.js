dc474549a07c034de28463ad25293ac184d3be76
Mane-Harshad <harshad.mane2108@gmail.com>
2026-03-29 13:46:26 +0530

    Solve getTheTitles exercise

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+ return books.map(book => book.title);
 
+ //2) return books.reduce((acc, book) => {
+ //    acc.push(book.title);
+ //    return acc;
+ //   }, []);
+
+ //3)  const titles = [];
+ //    for (let i = 0; i < books.length; i++)
+ //     {titles.push(books[i].title);}
+ //    return titles;
 };
 
 // Do not edit below this line
