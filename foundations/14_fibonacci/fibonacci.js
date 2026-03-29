7b4214616b924a779bbe9db9bce78a7d4665cdbb
Mane-Harshad <harshad.mane2108@gmail.com>
2026-03-29 12:35:45 +0530

    Solve fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+ member = Number(member);
+ if (member < 0)
+  return "OOPS";
+ if (member === 0)
+  return 0;
+ if (member === 1)
+  return 1;
+ return fibonacci(member - 1) + fibonacci(member - 2);
+// 2) if (member < 0)
+//     return "OOPS";
+//    if (member === 0)       
+//     return 0;
+//    if (member === 1)
+//     return 1;
+//    let a = 0;
+//    let b = 1;
+//    let c = 0;
+//    for (let i = 2; i <= member; i++)
+//     {c = a + b;
+//      a = b;
+//      b = c;}
+//    return c;
 };
 
 // Do not edit below this line
