b06f363433cc2038b3e96cc3ebf48e6c85d9b26e
Mane-Harshad <harshad.mane2108@gmail.com>
2026-03-29 21:00:25 +0530

    Solve findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+ const oldest = people.reduce((oldestPerson, currentPerson) => {
+  const oldestAge = (oldestPerson.yearOfDeath || new Date().getFullYear()) - oldestPerson.yearOfBirth;
+  const currentAge = (currentPerson.yearOfDeath || new Date().getFullYear()) - currentPerson.yearOfBirth;
+  return currentAge > oldestAge ? currentPerson : oldestPerson;
+ }, people[0]);
+ return oldest;
+
+// 2) people.sort((a, b) => {
+//     const ageA = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+//     const ageB = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+//     return ageB - ageA; // Sort in descending order
+//    });
+//    return people[0]; // Return the first person in the sorted array (the oldest)   
+
+// 3) let oldestPerson = people[0];
+//    for (let i = 1; i < people.length; i++)
+//    {const currentPerson = people[i];
+//     const oldestAge = (oldestPerson.yearOfDeath || new Date().getFullYear()) - oldestPerson.yearOfBirth;
+//     const currentAge = (currentPerson.yearOfDeath || new Date().getFullYear()) - currentPerson.yearOfBirth;
+//     if (currentAge > oldestAge)
+//      {oldestPerson = currentPerson;}}  
+//    return oldestPerson;
+
+
+
+
+
+
 
 };
 
